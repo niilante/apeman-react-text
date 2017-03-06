d898b1d2efe3784ef4bd736b720ab283f7b1a21f
Taka Okunishi <okunishinishi@gmail.com>
2017-03-06 13:35:48 +0900

    Deploy Github pages

---
+++ demo/demo.component.jsx
@@ -0,0 +1,32 @@
+'use strict'
+
+import React from 'react'
+import ApText from '../../lib/ap_text'
+import ApTextStyle from '../../lib/ap_text_style'
+
+export default React.createClass({
+  getInitialState () {
+    return {
+      value: 'ban'
+    }
+  },
+  handleChange (e) {
+    const s = this
+    s.setState({
+      value: e.target.value
+    })
+  },
+  render () {
+    const s = this
+    let { state } = s
+    return (
+      <div>
+        <ApTextStyle highlightColor="#b35600"/>
+        <ApText onChange={ s.handleChange } value={ state.value } onEnter={ () => console.log('enter') }/>
+        <ApText onChange={ s.handleChange } value={ state.value } rows={ 6 }/>
+        <ApText onChange={ s.handleChange } value={ state.value }
+                candidates={ [ 'banana', 'orange', 'apple' ] }/>
+      </div>
+    )
+  }
+})
